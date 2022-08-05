f66315558fc3585433d3e46ea6c7339dcd6acf39
hackedlings <pks9862728888@gmail.com>
2022-08-05 14:42:56 +0530

    Initial commit

---
+++ src/main/java/com/demo/springcloudeurekadiscoveryserverdemo/SpringCloudEurekaDiscoveryServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springcloudeurekadiscoveryserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaDiscoveryServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaDiscoveryServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springcloudeurekadiscoveryserverdemo/SpringCloudEurekaDiscoveryServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springcloudeurekadiscoveryserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaDiscoveryServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
